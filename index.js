8cb2887a21f2cef1b846a5c936a54b4c55e44980
Nowak Grzegorz <grzegorz.nowak@microsoft.wsei.edu.pl>
2018-10-23 16:47:26 +0200

    koniec zajęć; TODO: uzupełnić klawisze + dodać nagrywanie

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 document.body.addEventListener('keypress', (e)=> {
     console.log(e);
 
-    let sC = document.querySelector('#sC');
+    switch(e.key){
 
-    sC.currentTime = 0;
-    sC.play();
+        case 'w' :
+            playSound("c");
+            break;
+
+        case '1' :
+            playSound("cS");
+            break;
+
+
+    }
+
+
+
 })
 
 // tablica w kotrej łapiemy czas odpalenia przycisku oraz id dzwieku
 // settimeout( czas odpalenia dzwieku )
 
 // Date.now() 
+
+function playSound(id) {
+    let idS = "#" + id;
+
+    KeySound = document.querySelector(idS);
+
+    KeySound.currentTime = 0;
+    KeySound.play();
+}
